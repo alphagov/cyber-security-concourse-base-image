9b659c9d50ac65b476566113c421c6a19b629703
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2021-06-18 14:44:14 +0100

    Switch amazonlinux version to use tfenv and set default version

--- amazonlinux2/install_terraform.sh
+++
@@ -1,9 +0,0 @@
-cd /tmp
-curl https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip -o terraform_${TF_VERSION}_linux_amd64.zip
-curl https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_{$TF_VERSION}_SHA256SUMS -o terraform.sha
-if [ $(sha256sum -c terraform.sha 2>/dev/null | grep OK | wc -l) -eq 1 ]; then
-    echo 'Terraform file integrity is good'
-    unzip terraform_${TF_VERSION}_linux_amd64.zip
-    mv terraform /usr/bin/terraform
-    rm terraform_${TF_VERSION}_linux_amd64.zip terraform.sha
-fi
