d4632eeceb33eb5c3e70f6e77bad0e1c27b5a309
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-02 18:14:19 +0100

    Add the codebuild_image variable

--- terraform/modules/codepipeline/module-codebuild-self-update.tf
+++ terraform/modules/codepipeline/module-codebuild-self-update.tf
@@ -4,4 +4,5 @@ module "codebuild-self-update" {
   deployment_account_id             = data.aws_caller_identity.current.account_id
   deployment_role_name              = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
   terraform_directory               = "terraform/deployments/${data.aws_caller_identity.current.account_id}"
+  codebuild_image                   = "gdscyber/cyber-security-concourse-base-image:latest"
 }
