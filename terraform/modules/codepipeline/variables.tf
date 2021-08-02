c4ac8b3248dd206878be4e1d7450d11305d3e8cf
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-02 16:37:40 +0100

    added module for codebuild self update

--- terraform/modules/codepipeline/variables.tf
+++ terraform/modules/codepipeline/variables.tf
@@ -6,3 +6,8 @@ variable "staging_account_id" {
 variable "codestar_connection_id" {
   type = string
 }
+
+variable "codebuild_service_role_name" {
+  description = "the role code build uses to access other AWS services"
+  type = string
+}
