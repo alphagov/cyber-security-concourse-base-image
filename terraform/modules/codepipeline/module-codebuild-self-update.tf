1438df7e286d5ed8f723b50734b4c2fddd21e033
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-03 12:39:12 +0100

    Terraform fmt

--- terraform/modules/codepipeline/module-codebuild-self-update.tf
+++ terraform/modules/codepipeline/module-codebuild-self-update.tf
@@ -1,8 +1,8 @@
 module "codebuild-self-update" {
-  source                            = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild_terraform_apply?ref=codebuild-terraform-apply"
-  codebuild_service_role_name       = var.codebuild_service_role_name
-  deployment_account_id             = data.aws_caller_identity.current.account_id
-  deployment_role_name              = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
-  terraform_directory               = "terraform/deployments/${data.aws_caller_identity.current.account_id}"
-  codebuild_image                   = "gdscyber/cyber-security-concourse-base-image:latest"
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild_terraform_apply?ref=codebuild-terraform-apply"
+  codebuild_service_role_name = var.codebuild_service_role_name
+  deployment_account_id       = data.aws_caller_identity.current.account_id
+  deployment_role_name        = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
+  terraform_directory         = "terraform/deployments/${data.aws_caller_identity.current.account_id}"
+  codebuild_image             = "gdscyber/cyber-security-concourse-base-image:latest"
 }
