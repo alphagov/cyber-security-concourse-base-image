1fd69b1f78e397b4e636754585fa088e9064a1ec
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-04 16:48:16 +0100

    Add docker credentials name to module

--- terraform/deployments/670214072732/module_codepipeline.tf
+++ terraform/deployments/670214072732/module_codepipeline.tf
@@ -1,7 +1,8 @@
 module "codepipeline" {
-  source                      = "../../modules/codepipeline"
-  codestar_connection_id      = "51c5be90-8c8f-4d32-8be4-18b8f05c802c"
-  staging_account_id          = "103495720024"
-  codebuild_service_role_name = "CodePipelineExecutionRole"
-  environment                 = "test"
+  source                         = "../../modules/codepipeline"
+  codestar_connection_id         = "51c5be90-8c8f-4d32-8be4-18b8f05c802c"
+  staging_account_id             = "103495720024"
+  codebuild_service_role_name    = "CodePipelineExecutionRole"
+  environment                    = "test"
+  docker_credentials_secret_name = "docker_hub_credentials"
 }
