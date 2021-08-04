6af37b34b2e4a14bac52030c76c3b5158001b56d
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-04 15:22:49 +0100

    Update path to CodeBuild Terraform Apply module

--- terraform/modules/codepipeline/module-codebuild-self-update.tf
+++ terraform/modules/codepipeline/module-codebuild-self-update.tf
@@ -1,5 +1,5 @@
 module "codebuild-self-update" {
-  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild_terraform_apply?ref=codebuild-terraform-apply"
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_terraform_apply?ref=codebuild-terraform-apply"
   codebuild_service_role_name = var.codebuild_service_role_name
   deployment_account_id       = data.aws_caller_identity.current.account_id
   deployment_role_name        = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
