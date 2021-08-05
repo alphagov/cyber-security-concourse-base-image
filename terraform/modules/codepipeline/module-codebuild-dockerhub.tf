b3e9ceaede0917c71b8372257b7e02c6185e5322
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-05 11:24:56 +0100

    Update Docker Hub module according to recent changes to its interface

--- terraform/modules/codepipeline/module-codebuild-dockerhub.tf
+++ terraform/modules/codepipeline/module-codebuild-dockerhub.tf
@@ -1,7 +1,7 @@
 module "codebuild-dockerhub-build" {
   source              = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_project_container_build_docker_hub?ref=codebuild_container"
   service_role_name   = var.codebuild_service_role_name
-  image_name          = "cyber-security-concourse-base-image"
+  docker_hub_repo     = "gdscyber/cyber-security-concourse-base-image"
   image_tag           = "code_pipeline_test"
   build_context       = "."
   dockerfile          = "Dockerfile"
