bf8a27bacf51d7414f744ce14837b9279884cc74
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-06 11:20:20 +0100

    Add variable and temporarily switch branch for terraform-apply module

--- terraform/modules/codepipeline/module-codebuild-self-update.tf
+++ terraform/modules/codepipeline/module-codebuild-self-update.tf
@@ -1,5 +1,5 @@
 module "codebuild-self-update" {
-  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_terraform_apply"
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_terraform_apply?ref=codebuild-terraform-apply"
   codebuild_service_role_name = var.codebuild_service_role_name
   deployment_account_id       = data.aws_caller_identity.current.account_id
   deployment_role_name        = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
@@ -7,4 +7,5 @@ module "codebuild-self-update" {
   codebuild_image             = "gdscyber/cyber-security-concourse-base-image:latest"
   pipeline_name               = var.pipeline_name
   environment                 = var.environment
+  docker_hub_credentials      = "docker_hub_credentials"
 }
