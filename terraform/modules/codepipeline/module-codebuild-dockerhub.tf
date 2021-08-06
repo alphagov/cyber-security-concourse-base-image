6fdd4850a828325d8b3cc8417d3aeb4697cd95fc
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-06 10:44:09 +0100

    Change name of image and remove image_tag variable
    image_tag now defaults to `latest` and since we're changing the repo
    we're uploading to we don't have to worry about overwriting existing
    latest tagged images

--- terraform/modules/codepipeline/module-codebuild-dockerhub.tf
+++ terraform/modules/codepipeline/module-codebuild-dockerhub.tf
@@ -1,8 +1,7 @@
 module "codebuild-dockerhub-build" {
   source              = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_project_container_build_docker_hub?ref=codebuild_container"
   service_role_name   = var.codebuild_service_role_name
-  docker_hub_repo     = "gdscyber/cyber-security-concourse-base-image"
-  image_tag           = "code_pipeline_test"
+  docker_hub_repo     = "gdscyber/cyber-security-cd-base-image"
   build_context       = "."
   dockerfile          = "Dockerfile"
   docker_hub_username = var.docker_hub_username
