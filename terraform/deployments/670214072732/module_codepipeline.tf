2b2c765509fa0f13b6d4923960a822f54447dbfe
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2021-08-06 16:23:25 +0100

    Remove unused variable

--- terraform/deployments/670214072732/module_codepipeline.tf
+++ terraform/deployments/670214072732/module_codepipeline.tf
@@ -1,7 +1,6 @@
 module "codepipeline" {
   source                      = "../../modules/codepipeline"
   codestar_connection_id      = "51c5be90-8c8f-4d32-8be4-18b8f05c802c"
-  staging_account_id          = "103495720024"
   codebuild_service_role_name = "CodePipelineExecutionRole"
   environment                 = "test"
   docker_hub_credentials      = "docker_hub_credentials"
--- terraform/modules/codepipeline/variables.tf
+++ terraform/modules/codepipeline/variables.tf
@@ -1,8 +1,3 @@
-variable "staging_account_id" {
-  type        = string
-  description = "Test AWS account ID"
-}
-
 variable "codestar_connection_id" {
   type = string
 }
