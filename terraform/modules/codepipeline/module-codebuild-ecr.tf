e866b024c32be9917e6ec01d97f5df96e2702204
alice-carr <alice.bc.carr@gmail.com>
2021-08-09 15:18:06 +0100

    remove ref to ecr branch

--- terraform/modules/codepipeline/module-codebuild-ecr.tf
+++ terraform/modules/codepipeline/module-codebuild-ecr.tf
@@ -1,5 +1,5 @@
 module "codebuild-ecr" {
-  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_ECR_push?ref=codebuild-ecs"
+  source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_ECR_push"
   deployment_account_id       = data.aws_caller_identity.current.account_id
   deployment_role_name        = "CodePipelineDeployerRole_${data.aws_caller_identity.current.account_id}"
   codebuild_service_role_name = var.codebuild_service_role_name
