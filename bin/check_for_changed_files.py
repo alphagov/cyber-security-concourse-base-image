fc22abc2b59ec1f9914ee7d2f7c33f6cf8795dcd
alice-carr <alice.bc.carr@gmail.com>
2021-08-24 09:09:59 +0100

    check lists with startswith, to allow passing in floders rather than files

--- bin/check_for_changed_files.py
+++ bin/check_for_changed_files.py
@@ -1,27 +1,37 @@
 import json
 import os
 
+
 def get_json_from_file(file: str):
-    print(f"file: {file}")
     with open(file) as json_file:
         changed_files = json.load(json_file)
         return changed_files
 
-def write_json_to_file(file:str, data: bool):
-    print(f"file: {file}")
-    with open(file, 'w') as outfile:
+
+def write_json_to_file(file: str, data: bool):
+    with open(file, "w") as outfile:
         json.dump(data, outfile)
+
+
+def is_prefix_of_any(context_files, changed_files):
+    return any([
+        context_file
+        for context_file in context_files
+        if any(
+            changed_file.startswith(context_file) for changed_file in changed_files
+        )
+    ])
+
 
 def main():
     source_path = os.getenv("CODEBUILD_SRC_DIR")
-    print(f"source_path: {source_path}")
     git_diff_file = os.getenv("OUTPUT_FILENAME")
-    print(f"git_diff_file: {git_diff_file}")
     changed_files = get_json_from_file(git_diff_file)
 
     context_files = os.getenv("CONTEXT_FILE_LIST")
     context_files_updated = bool(set(changed_files).intersection(context_files))
 
     write_json_to_file(source_path + "/rebuild_task.json", context_files_updated)
+
 
 main()
