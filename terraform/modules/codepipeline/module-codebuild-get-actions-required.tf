4442987d5ecccd51366f2640489c028b7fbf1a5a
alice-carr <alice.bc.carr@gmail.com>
2021-09-01 11:39:13 +0100

    update actions required location

--- terraform/modules/codepipeline/module-codebuild-get-actions-required.tf
+++ terraform/modules/codepipeline/module-codebuild-get-actions-required.tf
@@ -1,4 +1,4 @@
-module "codebuild-git-diff" {
+module "codebuild-get-actions-required" {
   source                      = "github.com/alphagov/cyber-security-shared-terraform-modules//codebuild/codebuild_get_actions_required"
   codebuild_service_role_name = var.codebuild_service_role_name
   deployment_account_id       = data.aws_caller_identity.current.account_id
@@ -8,5 +8,5 @@ module "codebuild-git-diff" {
   environment                 = var.environment
   output_artifact_path        = "cd-images-pipeline/output-artifacts"
   artifact_bucket             = data.aws_s3_bucket.artifact_store.bucket
-  action_triggers             = "terraform/modules/codepipeline/action_triggers.json"
+  action_triggers             = "/terraform/modules/codepipeline/action_triggers.json"
 }
