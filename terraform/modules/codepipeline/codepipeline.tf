c9beb1b0295295e8d338eaa3a9711472007d4ed9
gds-ahine <aaron.hine@digital.cabinet-office.gov.uk>
2021-09-13 10:52:21 +0100

    CE-256 update pipeline name for base images

--- terraform/modules/codepipeline/codepipeline.tf
+++ terraform/modules/codepipeline/codepipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codepipeline" "cd-container-images" {
-  name     = "${var.pipeline_name}-${var.environment}"
+  name     = var.pipeline_name
   role_arn = data.aws_iam_role.pipeline_role.arn
   tags     = merge(local.tags, { Name = "${var.pipeline_name}-${var.environment}" })
 
